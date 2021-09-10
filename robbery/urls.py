7ea32919e4841ac8f54505e3176796e241cf7f3b
mohamedgamalmoha <62579977+mohamedgamalmoha@users.noreply.github.com>
2021-09-10 18:40:07 +0200

    Update urls.py

--- robbery/urls.py
+++ robbery/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('robbery/add', RobberyView.as_view(), name='robbery_add'),
     path('robbery/list', RobberyListView.as_view(), name='robbery_list'),
-    path('robbery/profile/<str:ip>', robbery_profile, name='robbery_profile'),
-    path('robbery/profile/<str:ip>/csv_export', export_csv, name='robbery_profile_export_csv'),
+    path('robbery/profile/<str:iban>', robbery_profile, name='robbery_profile'),
+    path('robbery/profile/<str:iban>/csv_export', export_csv, name='robbery_profile_export_csv'),
 ]
