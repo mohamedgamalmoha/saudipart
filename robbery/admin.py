90e07ff5c3a796a7f291ee83d3f671b709c4cfa0
mohamedgamalmoha <62579977+mohamedgamalmoha@users.noreply.github.com>
2021-09-14 11:51:36 +0200

    Delete robbery directory

--- robbery/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.models import User, Group
-
-from .models import Robbery
-
-
-admin.site.register(Robbery)
-admin.site.unregister(User)
-admin.site.unregister(Group)
